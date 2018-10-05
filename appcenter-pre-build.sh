7cde73609c77958c57411cbc036c72679dea6ae8
GPirrera <gpirrera@gmail.com>
2018-10-05 10:36:03 -0400

    update to app center prebuild script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+brew install node
+brew install watchman
+npm install -g react-native-cli
+npm install -g react-native-git-upgrade
+cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
+react-native-git-upgrade
+cd "${APPCENTER_SOURCE_DIRECTORY}"
+npm install
 cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 pod install
 cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
-#cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
-#react-native link
+cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
+react-native link
