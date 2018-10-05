d4535e12208ed7ee30b8c6869f73510458a27114
GPirrera <gpirrera@gmail.com>
2018-10-05 09:50:34 -0400

    Update appcenter-pre-build.sh to fix rnvideoplayer pbxproj file

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 pod install
+cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
 #cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 #react-native link
