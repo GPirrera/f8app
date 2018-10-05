b9c653ed140c5b24c60f17006d9013bab7e41334
GPirrera <gpirrera@gmail.com>
2018-10-05 13:09:23 -0400

    yet another change.

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,9 +6,9 @@ brew install node
 brew install watchman
 npm install -g react-native-cli
 npm install -g react-native-git-upgrade
-cd "${APPCENTER_SOURCE_DIRECTORY}"
-npm install --verbose
-react-native -v
+#cd "${APPCENTER_SOURCE_DIRECTORY}"
+#npm install --verbose
+#react-native -v
 #cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 #pod install
 #cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,8 +3,8 @@ set -ex
 cd "${APPCENTER_SOURCE_DIRECTORY}"
 printf "==========SOURCES DIRECTORY: ${PWD}=============="
 react-native-git-upgrade
-cd "${APPCENTER_SOURCE_DIRECTORY}"
-npm install --verbose
+#cd "${APPCENTER_SOURCE_DIRECTORY}"
+#npm install --verbose
 cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 pod install
 cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
