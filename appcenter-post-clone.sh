bf7aff7a624bfd4fff89dad7008ec14ece268b46
GPirrera <gpirrera@gmail.com>
2018-10-05 11:41:07 -0400

    post-clone and pre-build

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+brew update
+brew uninstall node@6
+brew install node
+brew install watchman
+npm install -g react-native-cli
+npm install -g react-native-git-upgrade
+cd "${APPCENTER_SOURCE_DIRECTORY}"
+printf "==========SOURCES DIRECTORY: ${PWD}=============="
+react-native-git-upgrade
+#npm install
+#cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
+#pod install
+#cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
+#cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
+#react-native link
