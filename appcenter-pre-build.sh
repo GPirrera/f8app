93bed26bd284906a7281852c8d3f8f0184bf2c13
GPirrera <gpirrera@gmail.com>
2018-10-05 12:45:41 -0400

    more build scripts fixed

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,7 +4,7 @@ cd "${APPCENTER_SOURCE_DIRECTORY}"
 printf "==========SOURCES DIRECTORY: ${PWD}=============="
 react-native-git-upgrade
 cd "${APPCENTER_SOURCE_DIRECTORY}"
-npm install
+npm install --verbose
 cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 pod install
 cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
