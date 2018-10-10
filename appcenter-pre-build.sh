18b14fc440cfafec80194b773b54847241aeda75
GPirrera <gpirrera@gmail.com>
2018-10-10 09:17:07 -0400

    uncommented a few lines in pre-build script.

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -9,7 +9,7 @@ set -ex
 #npm install --verbose
 #cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 #pod install
-#cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
+cp "${APPCENTER_SOURCE_DIRECTORY}/scripts/project.pbxproj" "${APPCENTER_SOURCE_DIRECTORY}/node_modules/react-native-native-video-player/ios/RNVideoPlayer.xcodeproj/project.pbxproj"
 #cd "${APPCENTER_SOURCE_DIRECTORY}/ios"
 #react-native link
 #npm start -- --reset-cache
